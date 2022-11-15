009c0754c3c003cf35505c6643f2942871c0c0df
Sky <ryudv>
2022-11-15 18:59:38 +0900

    스크롤,커서 커스텀 추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// 컨테이너1 메인화면 글자 타이핑 효과
+const content = "Hello, Welcome to My Portfolio Site:-) My Name is Haneul-Ryu, I'm Front-end Developer!";
+const introText = document.querySelector(".introText");
+let i = 0;
+
+function typing(){
+    let txt = content[i++];
+    introText.innerText += txt=== "\n" ? "<br/>": txt;
+    if (i > content.length) {
+        introText.textContent = "";
+        i = 0;
+    }
+}
+setInterval(typing, 200);
+
+// 컨테이너 박스 타이틀이 나타나는 효과
 let observer = new IntersectionObserver((e)=>{
     e.forEach((box)=>{
         if(box.isIntersecting) {
@@ -5,11 +21,12 @@ let observer = new IntersectionObserver((e)=>{
         } else {
             box.target.style.opacity = 0;
         }
-    })
-})
-let operBox = document.getElementsByClassName('operBox')
-//html요소를 감시해줌
-observer.observe(operBox[0])
-observer.observe(operBox[1])
-observer.observe(operBox[2])
-observer.observe(operBox[3])
+    });
+});
+let operBox = document.getElementsByClassName('operBox');
+
+observer.observe(operBox[0]);
+observer.observe(operBox[1]);
+observer.observe(operBox[2]);
+
+
