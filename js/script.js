c0d20d6aa68b62b7ffee6681aa123664e102baea
Sky <ryudv>
2022-11-22 02:12:34 +0900

    프로젝트화면 업데이트04

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // 컨테이너1 메인화면 글자 타이핑 효과
-const content = "Hello, Welcome to My Portfolio Site:-) My Name is Haneul-Ryu, I'm Front-end Developer!";
+const content = "Hello, Welcome to My\n Portfolio Site:-)\n My Name is Haneul-Ryu. I'm Front-end Developer!";
 const introText = document.querySelector(".introText");
 let i = 0;
 
 function typing(){
     let txt = content[i++];
-    introText.innerText += txt=== "\n" ? "<br/>": txt;
+    introText.innerText += txt;
     if (i > content.length) {
         introText.textContent = "";
         i = 0;
@@ -13,15 +13,17 @@ function typing(){
 }
 setInterval(typing, 100);
 
-// 컨테이너 박스 타이틀이 나타나는 효과
+// 컨테이너박스의 타이틀이 나타나는 효과
 let observer = new IntersectionObserver((e)=>{
     e.forEach((box)=>{
         if(box.isIntersecting) {
             box.target.style.opacity = 1;
+            box.target.style.transform = 'translateY(0)';
         } else {
             box.target.style.opacity = 0;
+            box.target.style.transform = 'translateY(100px)';
         }
-    });
+    }, { threshold: 1 });
 });
 let operBox = document.getElementsByClassName('operBox');
 
@@ -36,7 +38,9 @@ observer.observe(operBox[2]);
 const options = {
     root: null, // viewport
     rootMargin: "0px",
-    threshold: 1.0, // 50%가 viewport에 들어와 있어야 callback 실행
+    threshold: 1.0 
+     // threshold: 0.5 옵션을 전달해, 요소가 50% 이상 보일 때 콜백을 호출
+    // threshold의 범위는 0 ~ 1 (0% ~ 100%)
 }
 
 const observer1 = new IntersectionObserver(entries => {
@@ -68,46 +72,46 @@ boxList2.forEach(el => observer1.observe(el));
 
 
 
+// 스크롤 페이지 이동
+// window.onload = function(){
+//     const elm = document.querySelectorAll('.section');
+//     const elmCount = elm.length;
+//     elm.forEach(function(item, index){
+//       item.addEventListener('mousewheel', function(event){
+//         event.preventDefault();
+//         let delta = 0;
 
-window.onload = function(){
-    const elm = document.querySelectorAll('.section');
-    const elmCount = elm.length;
-    elm.forEach(function(item, index){
-      item.addEventListener('mousewheel', function(event){
-        event.preventDefault();
-        let delta = 0;
+//         if (!event) event = window.event;
+//         if (event.wheelDelta) {
+//             delta = event.wheelDelta / 120;
+//             if (window.opera) delta = -delta;
+//         } 
+//         else if (event.detail)
+//             delta = -event.detail / 3;
 
-        if (!event) event = window.event;
-        if (event.wheelDelta) {
-            delta = event.wheelDelta / 120;
-            if (window.opera) delta = -delta;
-        } 
-        else if (event.detail)
-            delta = -event.detail / 3;
+//         let moveTop = window.scrollY;
+//         let elmSelector = elm[index];
 
-        let moveTop = window.scrollY;
-        let elmSelector = elm[index];
-
-        // wheel down : move to next section
-        if (delta < 0){
-          if (elmSelector !== elmCount-1){
-            try{
-              moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-            }catch(e){}
-          }
-        }
+//         // wheel down : move to next section
+//         if (delta < 0){
+//           if (elmSelector !== elmCount-1){
+//             try{
+//               moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+//             }catch(e){}
+//           }
+//         }
         
-        // wheel up : move to previous section
-        else{
-          if (elmSelector !== 0){
-            try{
-              moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-            }catch(e){}
-          }
-        }
+//         // wheel up : move to previous section
+//         else{
+//           if (elmSelector !== 0){
+//             try{
+//               moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+//             }catch(e){}
+//           }
+//         }
 
-        const body = document.querySelector('html');
-        window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-      });
-    });
-  }
+//         const body = document.querySelector('html');
+//         window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+//       });
+//     });
+//   }
