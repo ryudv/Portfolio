16ea000397fb37bf7cf50e929d43b90a1ac51743
Sky <ryudv>
2023-02-09 18:01:41 +0900

    contact 화면 업데이트

--- js/script.js
+++ js/script.js
@@ -69,52 +69,116 @@ mainSlider.on('wheel', function(e) {
 
 
 
+(function () {
+    // get all data in form and return object
+    function getFormData(form) {
+      var elements = form.elements;
+      var honeypot;
 
+      var fields = Object.keys(elements).filter(function (k) {
+        if (elements[k].name === "honeypot") {
+          honeypot = elements[k].value;
+          return false;
+        }
+        return true;
+      }).map(function (k) {
+        if (elements[k].name !== undefined) {
+          return elements[k].name;
+          // special case for Edge's html collection
+        } else if (elements[k].length > 0) {
+          return elements[k].item(0).name;
+        }
+      }).filter(function (item, pos, self) {
+        return self.indexOf(item) == pos && item;
+      });
 
+      var formData = {};
+      fields.forEach(function (name) {
+        var element = elements[name];
 
+        // singular form elements just have one value
+        formData[name] = element.value;
 
-// 스크롤 페이지 이동
-// window.onload = function(){
-//     const elm = document.querySelectorAll('.section');
-//     const elmCount = elm.length;
-//     elm.forEach(function(item, index){
-//       item.addEventListener('mousewheel', function(event){
-//         event.preventDefault();
-//         let delta = 0;
+        // when our element has multiple items, get their values
+        if (element.length) {
+          var data = [];
+          for (var i = 0; i < element.length; i++) {
+            var item = element.item(i);
+            if (item.checked || item.selected) {
+              data.push(item.value);
+            }
+          }
+          formData[name] = data.join(', ');
+        }
+      });
 
-//         if (!event) event = window.event;
-//         if (event.wheelDelta) {
-//             delta = event.wheelDelta / 120;
-//             if (window.opera) delta = -delta;
-//         } 
-//         else if (event.detail)
-//             delta = -event.detail / 3;
+      // add form-specific values into the data
+      formData.formDataNameOrder = JSON.stringify(fields);
+      formData.formGoogleSheetName = form.dataset.sheet ||
+        "responses"; // default sheet name
+      formData.formGoogleSendEmail = form.dataset.email ||
+        ""; // no email by default
 
-//         let moveTop = window.scrollY;
-//         let elmSelector = elm[index];
+      return {
+        data: formData,
+        honeypot: honeypot
+      };
+    }
 
-//         // wheel down : move to next section
-//         if (delta < 0){
-//           if (elmSelector !== elmCount-1){
-//             try{
-//               moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-//             }catch(e){}
-//           }
-//         }
-        
-//         // wheel up : move to previous section
-//         else{
-//           if (elmSelector !== 0){
-//             try{
-//               moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-//             }catch(e){}
-//           }
-//         }
+    function handleFormSubmit(event) { // handles form submit without any jquery
+      event.preventDefault(); // we are submitting via xhr below
+      var form = event.target;
+      var formData = getFormData(form);
+      var data = formData.data;
 
-//         const body = document.querySelector('html');
-//         window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-//       });
-//     });
-//   }
+      // If a honeypot field is filled, assume it was done so by a spam bot.
+      if (formData.honeypot) {
+        return false;
+      }
 
+      disableAllButtons(form);
+      var url = form.action;
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      // xhr.withCredentials = true;
+      xhr.setRequestHeader("Content-Type",
+        "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          form.reset();
+          var formElements = form.querySelector(".form-elements")
+          if (formElements) {
+            formElements.style.display = "none"; // hide form
+          }
+          var thankYouMessage = form.querySelector(
+            ".thankyou_message");
+          if (thankYouMessage) {
+            thankYouMessage.style.display = "block";
+            Swal.fire('Completed the mail:-)');
+          }
+        }
+      };
+      // url encode form data for sending as post data
+      var encoded = Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(
+          data[k]);
+      }).join('&');
+      xhr.send(encoded);
+    }
 
+    function loaded() {
+      // bind to the submit event of our form
+      var forms = document.querySelectorAll("form.gform");
+      for (var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener("submit", handleFormSubmit, false);
+      }
+    };
+    document.addEventListener("DOMContentLoaded", loaded, false);
+
+    function disableAllButtons(form) {
+      var buttons = form.querySelectorAll("button");
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+    }
+  })();
